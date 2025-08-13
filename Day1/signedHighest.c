3483bc40d9b5402ae44a2c650a8d25b769a38463
SilentEngineer16 <kulkarni04.amrita@gmail.com>
2025-08-13 23:26:45 +0530

    Create signedHighest.c

---
+++ Day1/signedHighest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+long long int max = (long long int)(pow(2,63)-1);
+printf("highest number represented by signed long long int is %lld\n", max);
+return 0;
+}
